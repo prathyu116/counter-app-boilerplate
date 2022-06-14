57d744b3c10d46d0e6e492bb6541e63fc2eb99a2
prathyu Prasad <prathyuprasad04669@gmail.com>
2022-06-14 19:19:35 +0530

    complete

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
+import { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [first, setfirst] = useState(0);
+  console.log(first);
   return (
     <div className="App">
-      <h2 data-testid="counter-value">X</h2>
-      <button data-testid="counter-decrement-button">+</button>
-      <button data-testid="counter-increment-button">-</button>
+      <h2 data-testid="counter-value">{first}</h2>
+      <button
+        data-testid="counter-decrement-button"
+        onClick={() => {
+          setfirst(first + 1);
+        }}
+      >
+        +
+      </button>
+      <button disabled={first===0}
+        data-testid="counter-increment-button"
+        onClick={() => {
+          // if (first <= 0) {
+          //   return;
+          // }
+          setfirst(first - 1);
+        }}
+      >
+        -
+      </button>
     </div>
   );
 }
