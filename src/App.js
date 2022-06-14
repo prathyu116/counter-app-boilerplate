a50a12dc5cda405b0a6b16b5c24732b27bdc1f71
prathyu Prasad <prathyuprasad04669@gmail.com>
2022-06-14 18:29:44 +0530

    add

--- src/App.js
+++ src/App.js
@@ -4,8 +4,8 @@ function App() {
   return (
     <div className="App">
       <h2 data-testid="counter-value">X</h2>
-      <button data-testid="counter-decrement-button"></button>
-      <button data-testid="counter-increment-button"></button>
+      <button data-testid="counter-decrement-button">+</button>
+      <button data-testid="counter-increment-button">-</button>
     </div>
   );
 }
