493bce4312bf5d1440774070dd79260d5669af78
prathyu Prasad <prathyuprasad04669@gmail.com>
2022-06-14 19:47:12 +0530

    complete

--- src/App.js
+++ src/App.js
@@ -8,23 +8,21 @@ function App() {
     <div className="App">
       <h2 data-testid="counter-value">{first}</h2>
       <button
+        disabled={first === 0}
         data-testid="counter-decrement-button"
+        onClick={() => {
+          setfirst(first - 1);
+        }}
+      >
+        -
+      </button>
+      <button
+        data-testid="counter-increment-button"
         onClick={() => {
           setfirst(first + 1);
         }}
       >
         +
-      </button>
-      <button disabled={first===0}
-        data-testid="counter-increment-button"
-        onClick={() => {
-          // if (first <= 0) {
-          //   return;
-          // }
-          setfirst(first - 1);
-        }}
-      >
-        -
       </button>
     </div>
   );
